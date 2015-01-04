5980788579022be20cb751a238647613e85184b9
gtg489w <gtg489w@gmail.com>
2015-01-04 02:28:06 -0800

    Almost done checkout process

--- sitter-mobile-app/platforms/ios/www/js/index.js
+++ sitter-mobile-app/platforms/ios/www/js/index.js
@@ -1,37 +1,87 @@
 // ESRI STUFF
 
 var map;
-require([
-    "esri/map",
-    "esri/layers/FeatureLayer",
-    "esri/symbols/PictureMarkerSymbol",
-    "esri/symbols/SimpleFillSymbol",
-    "esri/Color", 
-    "dojo/domReady!",
-    ], function(Map, FeatureLayer, PictureMarkerSymbol, SimpleFillSymbol, Color) {
-    map = new Map("map", {
-        basemap: "streets",
-        center: [-115.1480962, 36.1714265], // longitude, latitude
-        zoom: 13
+
+setTimeout(function() {
+    require([
+        "esri/map",
+        "esri/layers/FeatureLayer",
+        "esri/symbols/PictureMarkerSymbol",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/Color", 
+        "dojo/domReady!",
+        ], function(Map, FeatureLayer, PictureMarkerSymbol, SimpleFillSymbol, Color) {
+        map = new Map("map", {
+            basemap: "streets",
+            center: [-115.1480962, 36.1714265], // longitude, latitude
+            zoom: 13
+        });
+
+        var sittersLayer = new FeatureLayer("http://services.arcgis.com/T4ElzZ5y0uIajYQQ/arcgis/rest/services/sitters/FeatureServer/0",{
+            //infoTemplate: template,
+            outFields: ["*"]
+        });
+
+        //var pictureMarkerSymbol = new PictureMarkerSymbol('http://www.esri.com/graphics/aexicon.jpg', 51, 51);
+        var selectionSymbol = new SimpleFillSymbol().setColor(new Color([255,255,0,0.5]));
+        sittersLayer.setSelectionSymbol(selectionSymbol);
+
+
+        sittersLayer.on('click', function(e) {
+            if(e.graphic.attributes.sitter_id == 1) {
+                showJeremy();
+            } else {
+                showAmanda();
+            }
+            $('#map_profile').fadeIn();
+        });
+        map.addLayer(sittersLayer);
+
     });
+}, 1000);
 
-    var sittersLayer = new FeatureLayer("http://services.arcgis.com/T4ElzZ5y0uIajYQQ/arcgis/rest/services/sitters/FeatureServer/0",{
-        //infoTemplate: template,
-        outFields: ["*"]
+var showJeremy = function() {
+    
+    $.ajax({
+        url: "https://proapi.whitepages.com/2.0/phone.json?phone_number=8643568373&api_key=1178a3b580cea98d66fc73e233f344b3",
+        context: document.body
+    }).success(function(response) {
+        console.log(response);
     });
+    $('#video-bio').show();
+    $('#value-bio').text("Hi! I'm Jeremy. I look forward to serving you.");
+    $('#value-sits').text('1');
+    $('#value-age').text('37');
+    $('#whitepages_verified').hide();
+    $('#filter-baby').removeClass('unavailable');
+    $('#filter-pet').addClass('unavailable');
+    $('#filter-house').addClass('unavailable');
+    $('#value-name').text('Jeremy L.');
+    $('.profile-photo-round').css('background-image','url(img/jeremy.jpg)');
+    $('.profile-photo-backdrop').css('background-image','url(img/jeremy.jpg)');
+    $('#fifthstar').hide();
+};
 
-    //var pictureMarkerSymbol = new PictureMarkerSymbol('http://www.esri.com/graphics/aexicon.jpg', 51, 51);
-    var selectionSymbol = new SimpleFillSymbol().setColor(new Color([255,255,0,0.5]));
-    sittersLayer.setSelectionSymbol(selectionSymbol);
-
-
-    sittersLayer.on('click', function(e) {
-        console.log(e.graphic.attributes.sitter_id);
-        $('#map_profile').fadeIn();
+var showAmanda = function() {
+    $.ajax({
+        url: "https://proapi.whitepages.com/2.0/phone.json?phone_number=5708671755&api_key=1178a3b580cea98d66fc73e233f344b3",
+        context: document.body
+    }).success(function(response) {
+        console.log(response);
     });
-    map.addLayer(sittersLayer);
-
-});
+    $('#video-bio').hide();
+    $('#value-bio').text("Thanks for visiting my profile! I'm Amanda and I've been a nanny and babysitter for a number of years.");
+    $('#value-sits').text('47');
+    $('#value-age').text('28');
+    $('#whitepages_verified').show();
+    $('#filter-baby').removeClass('unavailable');
+    $('#filter-pet').removeClass('unavailable');
+    $('#filter-house').removeClass('unavailable');
+    $('#value-name').text('Amanda S.');
+    $('.profile-photo-round').css('background-image','url(img/amanda.jpg)');
+    $('.profile-photo-backdrop').css('background-image','url(img/amanda.jpg)');
+    $('#fifthstar').show();
+};
 
 
 // MOBILE APP CODE
@@ -65,7 +115,6 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        alert('here!');
     }
 };
 
--- sitter-mobile-app/www/js/index.js
+++ sitter-mobile-app/www/js/index.js
@@ -1,41 +1,44 @@
 // ESRI STUFF
 
 var map;
-require([
-    "esri/map",
-    "esri/layers/FeatureLayer",
-    "esri/symbols/PictureMarkerSymbol",
-    "esri/symbols/SimpleFillSymbol",
-    "esri/Color", 
-    "dojo/domReady!",
-    ], function(Map, FeatureLayer, PictureMarkerSymbol, SimpleFillSymbol, Color) {
-    map = new Map("map", {
-        basemap: "streets",
-        center: [-115.1480962, 36.1714265], // longitude, latitude
-        zoom: 13
+
+setTimeout(function() {
+    require([
+        "esri/map",
+        "esri/layers/FeatureLayer",
+        "esri/symbols/PictureMarkerSymbol",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/Color", 
+        "dojo/domReady!",
+        ], function(Map, FeatureLayer, PictureMarkerSymbol, SimpleFillSymbol, Color) {
+        map = new Map("map", {
+            basemap: "streets",
+            center: [-115.1480962, 36.1714265], // longitude, latitude
+            zoom: 13
+        });
+
+        var sittersLayer = new FeatureLayer("http://services.arcgis.com/T4ElzZ5y0uIajYQQ/arcgis/rest/services/sitters/FeatureServer/0",{
+            //infoTemplate: template,
+            outFields: ["*"]
+        });
+
+        //var pictureMarkerSymbol = new PictureMarkerSymbol('http://www.esri.com/graphics/aexicon.jpg', 51, 51);
+        var selectionSymbol = new SimpleFillSymbol().setColor(new Color([255,255,0,0.5]));
+        sittersLayer.setSelectionSymbol(selectionSymbol);
+
+
+        sittersLayer.on('click', function(e) {
+            if(e.graphic.attributes.sitter_id == 1) {
+                showJeremy();
+            } else {
+                showAmanda();
+            }
+            $('#map_profile').fadeIn();
+        });
+        map.addLayer(sittersLayer);
+
     });
-
-    var sittersLayer = new FeatureLayer("http://services.arcgis.com/T4ElzZ5y0uIajYQQ/arcgis/rest/services/sitters/FeatureServer/0",{
-        //infoTemplate: template,
-        outFields: ["*"]
-    });
-
-    //var pictureMarkerSymbol = new PictureMarkerSymbol('http://www.esri.com/graphics/aexicon.jpg', 51, 51);
-    var selectionSymbol = new SimpleFillSymbol().setColor(new Color([255,255,0,0.5]));
-    sittersLayer.setSelectionSymbol(selectionSymbol);
-
-
-    sittersLayer.on('click', function(e) {
-        if(e.graphic.attributes.sitter_id == 1) {
-            showJeremy();
-        } else {
-            showAmanda();
-        }
-        $('#map_profile').fadeIn();
-    });
-    map.addLayer(sittersLayer);
-
-});
+}, 1000);
 
 var showJeremy = function() {
     
@@ -43,7 +46,7 @@ var showJeremy = function() {
         url: "https://proapi.whitepages.com/2.0/phone.json?phone_number=8643568373&api_key=1178a3b580cea98d66fc73e233f344b3",
         context: document.body
     }).success(function(response) {
-        console.log(response);
+        //console.log(response);
     });
     $('#video-bio').show();
     $('#value-bio').text("Hi! I'm Jeremy. I look forward to serving you.");
@@ -64,7 +67,7 @@ var showAmanda = function() {
         url: "https://proapi.whitepages.com/2.0/phone.json?phone_number=5708671755&api_key=1178a3b580cea98d66fc73e233f344b3",
         context: document.body
     }).success(function(response) {
-        console.log(response);
+        //console.log(response);
     });
     $('#video-bio').hide();
     $('#value-bio').text("Thanks for visiting my profile! I'm Amanda and I've been a nanny and babysitter for a number of years.");
@@ -107,12 +110,36 @@ var app = {
         $('#profile-close').click(function() {
             $('#map_profile').fadeOut();
         });
+        $('#book-button').click(function() {
+            $('#page_map').hide();
+            $('#page_access').fadeIn();
+        });
+        $('button.toggle').click(function() {
+            if($(this).hasClass('active')) {
+                $(this).removeClass('active');
+            } else {
+                $(this).addClass('active');
+            }
+        });
+        $('#button-bookit').click(function() {
+            $('#page_access').hide();
+            $('#page-section-booked').hide();
+            $('#page-section-booking').show();
+            $('#page_booking').show();
+            setTimeout(function() {
+                $('#page-section-booking').hide();
+                $('#page-section-booked').show();
+                setTimeout(function() {
+                    $('#page_booking').hide();
+                    $('#page_receipt').show();
+                }, 3000);
+            }, 3000);
+        });
     },
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        alert('here!');
     }
 };
 
